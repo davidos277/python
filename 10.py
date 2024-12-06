b5ce4e1ac4a3afcd3dbeade143e3322e9df4162f
davidos277 <davidmanagarov31@gmail.com>
2024-12-06 07:38:01 +0300

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,6 @@
+file_vvod=open('C:\\Users\\David\\Desktop\\моя работа\python сдать\\10.работа\\Радинский Давид Антонович_УБ-41_vvod.txt','r')
+print(file_vvod.read(252))
+file_vvod.close
+file_vivod=open('C:\\Users\\David\\Desktop\\моя работа\\python сдать\\10.работа\\Радиснкий Давид Антонович_УБ-41_vivod.txt','r')
+print(file_vivod.read(268))
+file_vivod.close
